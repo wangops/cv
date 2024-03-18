39974d1e9e91b863f032f296f95b4e2c3bed71b5
wanghexiang <337192133@qq.com>
2024-03-18 23:23:19 +0800

    gl

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
-
+import webgl from './webgl'
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        {
+    routes: [{
             path: '/',
             name: 'index',
             component: Index
-        }
+        },
+        ...webgl
     ]
 })
 export default router
---
+++ src/router/webgl.ts
@@ -0,0 +1,8 @@
+
+import Chapter01 from "@/views/webgl/Chapter01.vue"
+export default [{
+        path: '/webgl/chapter01',
+        name: 'chapter01',
+        component: Chapter01
+    }
+]
